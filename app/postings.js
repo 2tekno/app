e23da60f6a9640220554f0fc642ca9074a4c15d4
unknown <2tekno@gmail.com>
2018-07-14 17:51:09 -0700

    notes here

--- app/postings.js
+++ app/postings.js
@@ -646,7 +646,7 @@ function getIp () {
 			if (alias >= 1) {
 			// this single interface has multiple ipv4 addresses
 				console.log(ifname + ':' + alias, iface.address);
-				ip += iface.address + ' ';
+				ip += iface.address + ' | ';
 			} else {
 			// this interface has only one ipv4 adress
 				console.log(ifname, iface.address);
